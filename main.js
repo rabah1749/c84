7fabd4bae054af2aadb5c9dbab2b555474eb7078
rabah1749 <77684146+rabah1749@users.noreply.github.com>
2021-06-10 19:47:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+canvas=document.getElementById("myCanvas"); 
+ctx=canvas.getContext("2d");
+rover_width=100;
+rover_height=90;
+rover_x=10;
+rover_y=10;
+background_img="mars.jpg";
+rover_img="rover.png";
+
+function add(){
+    background_imgtag=new Image();
+    background_imgtag.onload=upbackground;
+    background_imgtag.src=background_img;
+
+    rover_imgtag=new Image();
+     rover_imgtag.onload=uprover;
+    rover_imgtag.src=rover_img;
+}
+
+function upbackground(){
+    ctx.drawImage(background_imgtag,0,0,canvas.width,canvas.height);
+
+}
+function uprover(){
+    ctx.drawImage(rover_imgtag,rover_x,rover_y,rover_width,rover_height);
+
+}
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keypress=e.keyCode;
+    if(keypress=='38'){
+        up();
+    }
+    if(keypress=='40'){
+        down();
+    }
+    if(keypress=='37'){
+        left();
+    }
+    if(keypress=='39'){
+        right();
+    }
+}
